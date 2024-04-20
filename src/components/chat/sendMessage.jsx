d34d406dbc00595a33552cf04fe7477d7a6ed988
meraj <merajpiri83@gmail.com>
2024-04-20 11:52:08 +0330

    add emoji to send message

--- src/components/chat/sendMessage.jsx
+++ src/components/chat/sendMessage.jsx
@@ -1,95 +1,102 @@
 //  style 
 import "../../static/chat/sendMessage.css";
 // get mode of background 
-import { useSelector , useDispatch  } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 // icons 
 import { FaRegFaceSmile } from "react-icons/fa6";
 import { FaPlus } from "react-icons/fa";
 import { HiMicrophone } from "react-icons/hi2";
 import { PiTelegramLogoBold } from "react-icons/pi";
 // animation 
-import { Fade } from "react-awesome-reveal";
+import { Fade, Slide } from "react-awesome-reveal";
 import { useEffect, useRef, useState } from "react";
 // API 
-import API,{setAccessWhen401,getCurrentUser} from "../../authentication/auth";
+import API, { setAccessWhen401, getCurrentUser } from "../../authentication/auth";
 // react toastify
 import { toast } from "react-toastify";
 // change contact
-import {changeContact,contactToggle} from "../../reducers/contact";
-import {changeMessageToggle} from "../../reducers/message";
+import { changeContact, contactToggle } from "../../reducers/contact";
+import { changeMessageToggle } from "../../reducers/message";
 import { useLocation, useNavigate } from "react-router-dom";
 // react emoji
-import Picker from "emoji-picker-react";
+import InputEmoji from 'react-input-emoji'
 
 
 
 const SendMessage = () => {
 
     const mode = useSelector((state) => state.background.mode);
-    const [content,setContent] = useState("");
-    const contact = useSelector((state) => state.contact) ;
+    const [content, setContent] = useState("");
+    const contact = useSelector((state) => state.contact);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const location = useLocation();
-    const [currentUser,setCurrentUser] = useState({});
+    const [currentUser, setCurrentUser] = useState({});
     const chatType = useSelector((state) => state.page.chat_type);
-    const group = useSelector((state) => state.group );
- 
+    const group = useSelector((state) => state.group);
+    const [emoji, setEmoji] = useState(null);
+
     const sendData = async () => {
         const formData = new FormData();
-        formData.append("text",content)
+        formData.append("text", content)
 
-        if(chatType === "chat") {
-            await API.post(`/chat/${contact.chat_id}/message/create/`,formData).then((response) => {
-                const contact = response.data.with_who === currentUser ? response.data.create_by : response.data.with_who ;
-                dispatch(changeContact({"chat_id" : response.data.chat,...contact,...contact.profile}))
+        if (chatType === "chat") {
+            await API.post(`/chat/${contact.chat_id}/message/create/`, formData).then((response) => {
+                const contact = response.data.with_who === currentUser ? response.data.create_by : response.data.with_who;
+                dispatch(changeContact({ "chat_id": response.data.chat, ...contact, ...contact.profile }))
                 dispatch(changeMessageToggle());
-            }).catch((error)=>{ 
-                try{
-                    if(error.response.status === 401){
-                        setAccessWhen401(navigate,location.pathname,dispatch)
+            }).catch((error) => {
+                try {
+                    if (error.response.status === 401) {
+                        setAccessWhen401(navigate, location.pathname, dispatch)
                     }
                     toast.error(Object.values(error.response.data)[0][0])
-                }catch{}
+                } catch { }
             })
         }
 
-        if(chatType === "group") {
-            formData.append("group",group.id);
-            await API.post("/group/message/create/",formData).then((response) => {
+        if (chatType === "group") {
+            formData.append("group", group.id);
+            await API.post("/group/message/create/", formData).then((response) => {
                 console.log(response.data)
             }).catch((error) => {
-                try{
-                    if(error.response.status === 401){
-                        setAccessWhen401(navigate,location.pathname,dispatch)
+                try {
+                    if (error.response.status === 401) {
+                        setAccessWhen401(navigate, location.pathname, dispatch)
                     }
                     toast.error(Object.values(error.response.data)[0][0])
-                }catch{}
+                } catch { }
             })
         }
 
         setContent("");
         dispatch(contactToggle());
     };
-    
-    const emojiClicked = (event,emojiObject) => {
-        console.log(emojiObject);
-    };
 
     useEffect(() => {
-        getCurrentUser(setCurrentUser,navigate,location);
-    },[]);
- 
-    return (  
-        <Fade duration={300}> 
+        getCurrentUser(setCurrentUser, navigate, location);
+    }, []);
+
+    const onEnterHandeler = (text) => {
+        console.log("enterd")
+        sendData();
+    }
+
+
+    return (
+        <Fade duration={300}>
             <form className={"send-message " + mode}>
                 <PiTelegramLogoBold className="icon" onClick={() => sendData()} />
                 {/* <HiMicrophone className="icon" /> */}
-                <input type="text" className={"message-input " + mode} placeholder="پیام خود را اینجا بنویسید..."
-                onChange={(e) => setContent(e.target.value) } value={content} />
-                <Picker onEmojiClick={emojiClicked} />
+                <InputEmoji
+                    value={content}
+                    onChange={setContent}
+                    cleanOnEnter
+                    onEnter={onEnterHandeler}
+                    placeholder="پیام خود را اینجا بنویسید..."
+                    className={"message-input " + mode}
+                />
                 <FaPlus className="icon" />
-                {/* <FaRegFaceSmile className="icon" /> */}
             </form>
         </Fade>
     )
